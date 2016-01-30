39dd509f7274195daca4393de0460077b44bcae3
Tom Sundstrom <office@tomsun.ax>
2016-01-30 20:18:39 +0200

    Support non-interactive configuration of url scheme

--- install.sh
+++ install.sh
@@ -4,22 +4,25 @@ if [ "$PLUGIN_ENV" == "test" ]; then
   exit
 fi
 
-DEFAULT_SCHEME="spotify-cordova"
+if [ "$CORDOVA_SPOTIFY_URL_SCHEME" == "" ]
+then
+    DEFAULT_SCHEME="spotify-cordova"
 
-echo "The Spotify SDK Plugin needs a URL scheme for authentication."
-echo "See http://bit.ly/1u11ZUz for more information"
-printf "Specify your URL scheme [$DEFAULT_SCHEME]: "
+    echo "The Spotify SDK Plugin needs a URL scheme for authentication."
+    echo "See http://bit.ly/1u11ZUz for more information"
+    printf "Specify your URL scheme [$DEFAULT_SCHEME]: "
 
-read urlscheme
-
-if [ "$urlscheme" == "" ]; then
-  urlscheme="$DEFAULT_SCHEME"
+    read CORDOVA_SPOTIFY_URL_SCHEME
+    if [ "$CORDOVA_SPOTIFY_URL_SCHEME" == "" ]
+    then
+      CORDOVA_SPOTIFY_URL_SCHEME="$DEFAULT_SCHEME"
+    fi
 fi
 
 echo "Writing URL scheme to plugin.xml"
 
 mv plugins/com.timflapper.spotify/plugin.xml plugins/com.timflapper.spotify/plugin.bak.xml
-sed "s/{{URL_SCHEME}}/$urlscheme/g" plugins/com.timflapper.spotify/plugin.bak.xml > plugins/com.timflapper.spotify/plugin.xml
+sed "s/{{URL_SCHEME}}/$CORDOVA_SPOTIFY_URL_SCHEME/g" plugins/com.timflapper.spotify/plugin.bak.xml > plugins/com.timflapper.spotify/plugin.xml
 rm plugins/com.timflapper.spotify/plugin.bak.xml
 
 echo "Removing placeholder"
